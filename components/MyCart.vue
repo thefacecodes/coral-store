3ea850e5cfbb3837199b907c8c3e793864aa4d2f
unknown <ezekielabolade@gmail.com>
2022-11-25 18:17:05 +0100

    Update

--- components/MyCart.vue
+++ components/MyCart.vue
@@ -1,15 +1,149 @@
 <template>
-  <div>
-    <h2>My Cart</h2>
+  <div class="profileCart">
+    <empty-cart v-if="$store.state.bag.length < 1" />
+    <div class="cart" v-if="$store.state.bag.length > 0">
+      <h1>My Cart</h1>
+      <div class="container">
+        <div class="table-container">
+          <table>
+            <tbody>
+              <tr class="font-bold">
+                <td>Product</td>
+                <td>Price</td>
+                <td>Quantity</td>
+                <td>Total</td>
+              </tr>
+              <tr v-for="product in cartUpdate" :key="product.id">
+                <td>
+                  <nuxt-link :to="'/store/' + product.id">
+                    <div class="cart-item">
+                      <img
+                        :src="
+                          product.image
+                            ? require(`~/assets/images/${product.image}`)
+                            : product.thumbnail
+                        "
+                        alt=""
+                      />
+                      <div>
+                        <h4>{{ product.brand }}</h4>
+                        <p class="text-[#626262]">
+                          {{ product.title ? product.title : product.name }}
+                        </p>
+                      </div>
+                    </div>
+                  </nuxt-link>
+                </td>
+                <td>${{ product.price }}</td>
+                <td>{{ product.quantity }}</td>
+                <td>${{ (product.price * product.quantity).toFixed(2) }}</td>
+                <td>
+                  <button
+                    :disabled="product.quantity <= 1"
+                    @click="$store.commit('decreaseQuantity', product)"
+                    class="text-[#B00020] shadow p-4"
+                  >
+                    &#8211;
+                  </button>
+                  <button
+                    @click="$store.commit('increaseQuantity', product)"
+                    class="text-[#1B4B66] shadow p-4"
+                  >
+                    &#43;
+                  </button>
+                </td>
+                <td>
+                  <button
+                    @click="$store.commit('removeFromCart', product)"
+                    class="text-[#B00020] shadow p-4"
+                  >
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                  <button
+                    @click="$store.commit('addToWishlist', product)"
+                    class="text-[#1B4B66] shadow p-4"
+                  >
+                    <i class="fa-solid fa-heart"></i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="order">
+          <h2>Order Summary</h2>
+          <div>
+            <h3>Subtotal :</h3>
+            <h4>${{ $store.state.totalAmount }}</h4>
+          </div>
+          <div>
+            <h3>Delivery fee</h3>
+            <h4>$0</h4>
+          </div>
+          <div>
+            <h3>Discount</h3>
+            <h4>${{ $store.state.discount.toFixed(2) }}</h4>
+          </div>
+          <div>
+            <h3>Grand total</h3>
+            <h4>
+              ${{
+                ($store.state.totalAmount - $store.state.discount).toFixed(2)
+              }}
+            </h4>
+          </div>
+          <div>
+            <button
+              @click="$router.push('/checkout')"
+              class="bg-[#1B4B66] text-white"
+            >
+              Place Order
+            </button>
+            <button @click="$router.push('/store')" class="text-[#1B4B66]">
+              Continue Shopping
+            </button>
+          </div>
+        </div>
+      </div>
+      <form @submit.prevent="applyCoupon()">
+        <h5>Apply Coupon</h5>
+        <div class="forminput">
+          <input
+            type="text"
+            autocomplete="off"
+            placeholder="Apply Coupon Code"
+            v-model="coupon"
+            name="coupon"
+            id="coupon"
+          />
+          <button type="submit">CHECK</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      coupon: "",
+    };
+  },
+  computed: {
+    cartUpdate() {
+      return this.$store.state.bag;
+    },
+  },
+  methods: {
+    applyCoupon() {
+      this.$store.commit("applyCoupon", this.coupon);
+      this.coupon = "";
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- components/MyWishlist.vue
+++ components/MyWishlist.vue
@@ -1,15 +1,51 @@
 <template>
   <div>
-    <h1>My Wishlist</h1>
+    <empty-wishlist v-if="$store.state.wishlist.length < 1" />
+    <div class="myWishlist" v-if="$store.state.wishlist.length > 0">
+      <h1>My Wishlist</h1>
+      <div
+        class="wishlist-item"
+        v-for="product in $store.state.wishlist"
+        :key="product.id"
+      >
+        <nuxt-link :to="'/store/' + product.id">
+          <img
+            :src="
+              product.image
+                ? require(`~/assets/images/${product.image}`)
+                : product.thumbnail
+            "
+            alt=""
+          />
+        </nuxt-link>
+        <div class="details">
+          <nuxt-link :to="'/store/' + product.id">
+            <h2>{{ product.brand }}</h2>
+            <p>{{ product.name ? product.name : product.title }}</p>
+          </nuxt-link>
+          <h3>${{ product.price.toFixed(2) }}</h3>
+          <button
+            @click="$store.commit('removeFromWishlist', product)"
+            class="text-[#B00020]"
+          >
+            <i class="fa-solid fa-trash"></i> Remove
+          </button>
+          <br />
+          <button
+            @click="$store.commit('addToCart', product)"
+            class="text-[#1B4B66]"
+          >
+            <i class="fa-solid fa-bag-shopping"></i> Move to Cart
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
